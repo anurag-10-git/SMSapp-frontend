37221de67df0a320a5ab5b491000a4d059170dd7
anurag-10-git <anuragmehra2011@gmail.com>
2023-02-13 10:26:18 +0530

    commit changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import classes from './Header.module.css';
 
 const Header = () => {
   return (
-     <nav>
+     <nav className={classes.navbar}>
        <ul>
         <li>Home</li>
         <li>Messages </li>
